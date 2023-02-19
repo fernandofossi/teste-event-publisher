6dc4c9834ec8d9935b9c011fd5b40e091e6a5e60
Fernando Fossi <fernandofossi@gmail.com>
2023-02-18 21:42:25 -0300

    First Commit

---
+++ src/main/java/com/fossi/test/eventpublisher/TestEventPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.fossi.test.eventpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestEventPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestEventPublisherApplication.class, args);
+	}
+
+}
